87c607eaad3740194555c8d915b88ca3845ebf8c
cdavidson <chrisedavidson1@comcast.net>
2013-01-24 19:55:18 -0500

    Prelim testing with the views (working) with urls

--- MagicSet/views.py
+++ MagicSet/views.py
@@ -5,4 +5,4 @@ import datetime
 def current_datetime(request):
     now = datetime.datetime.now()
     html = "<html><body>It is now %s.</body></html>" %now
-    return HttpResponse
+    return HttpResponse(html)
